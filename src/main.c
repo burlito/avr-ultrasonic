ccc64284d08be6f835faab8bc30b65cb3837f8ab
Andrej Manduch <AManduch@gmail.com>
2014-03-28 23:23:05 +0100

    basic driver

--- src/main.c
+++ src/main.c
@@ -7,32 +7,49 @@
 
 void USARTInit(uint16_t ubrr_value)
 {
-   /* Set Baud rate*/
-   UBRR0L = ubrr_value;                     /* 0b01100111*/
-   UBRR0H = (ubrr_value>>8);               /* 0b00000000*/
-   
-   /* Enable The receiver and transmitter*/
-   UCSR0B = _BV(TXEN0);                     /* (1 << RXEN0)|(1 << TXEN0);*/
-   
-   /* Set Frame Format; Asynchronous mode, No Parity, 1 StopBit, char size 8*/
-   UCSR0C = _BV(UCSZ00)|_BV(UCPHA0);          /* (0b11<<UCSZ0)*/
+	UBRR0L = ubrr_value;
+	UBRR0H = (ubrr_value>>8);
+	UCSR0B = _BV(TXEN0);
+	UCSR0C = _BV(UCSZ00)|_BV(UCSZ01);
 }
 
 void USARTWriteChar(uint8_t data)
 {
-    /* Wait until the transmitter is ready */
-    while(((UCSR0A & (1<<UDRE0))==0)){
-    }
-    /* Now write the data to USART buffer */
-    UDR0=data;
+	while (((UCSR0A & (1<<UDRE0)) == 0))
+		;
+	UDR0 = data;
 }
 
-int main (void)
-{
-    xfunc_out = USARTWriteChar;
+/**/
+int b_messure(void){
+	int time = 0;
 
-    USARTInit(12);
-    while(1) 
-    {
-    }
+	DDRC |= _BV(DDC0);
+	DDRC &= ~_BV(PORTC1);
+	PORTC |= _BV(PORTC0);
+	_delay_us(20);
+	PORTC &= ~_BV(PORTC0);
+	while (!(PINC & _BV(PINC1)))
+		;
+
+	while (PINC & _BV(PINC1)) {
+		_delay_us(10);
+		time++;
+	}
+
+	return time;
+}
+
+int main(void)
+{
+	xfunc_out = USARTWriteChar;
+
+	USARTInit(12);
+	while (1) {
+		_delay_ms(250);
+		xprintf(PSTR("nameral: %d\n"), b_messure());
+	}
+
+	/*this should be unreachable*/
+	return 0;
 }
